cf79fae832555432168fedae5981c6061aa170b8
moh1415 <moh14151415@gmail.com>
2020-02-03 16:28:20 +0300

    The required requirements done;

--- main1.js
+++ main1.js
@@ -23,25 +23,32 @@ const start = function(){
 
     if(checkWin() == "X")
     {
+        msgDiv.innerText ="";
+        
         msg1Div.innerText="You Win player 1 ";
        
     }
     else if(checkWin() == "O"){
+        msgDiv.innerText="";
         msg1Div.innerText="You Win player 2 ";
        
     }
     else{
-       // let msg1Div = document.querySelector('.msg1').visibility = "hidden";
-        for(let i =0;i<boxs.length;i++){
-        if(boxs[i].innerText != "" && boxs[8].innerText !="")
+       //check if all boxs have value not empty and no match and print
+        for(let i =0;i<$(boxs).length;i++){
+        if($(boxs[i]).text() != "" && notempty($(boxs[8]).text() ,$(boxs[0]).text(),$(boxs[1]).text()) &&
+        notempty($(boxs[2]).text() ,$(boxs[3]).text(),$(boxs[4]).text()) && notempty($(boxs[5]).text() ,$(boxs[6]).text(),$(boxs[7]).text()) )
         {
-            
+            msgDiv.innerText="";
             let msg1Div = document.querySelector('.msg1');
             msg1Div.innerText="Tie";
+            
         }
            
-            
-    
+        // boxs[8].innerText !="" && boxs[0].innerText !="" && boxs[1].innerText !=""&& boxs[2].innerText !="" && boxs[3].innerText !="" && boxs[4].innerText !="" && boxs[5].innerText !="" && boxs[6].innerText !="" && boxs[7].innerText !=""
+    // notempty($(boxs[8]).text() ,$(boxs[0]).text(),$(boxs[1]).text())
+    // notempty($(boxs[2]).text() ,$(boxs[3]).text(),$(boxs[4]).text())
+    // notempty($(boxs[5]).text() ,$(boxs[6]).text(),$(boxs[7]).text())
          }
         
     }
@@ -55,17 +62,14 @@ function fisrt_role(player1)
         {
             if(player1 == "X")
             {
-               
-                //msgDiv.innerText ="player 1 Your turn choose "+player1;
-                $(event.target).text(player1);
+                $(event.target).text(player1).animate({fontSize: "6em"}).css('backgroundImage', 'url(images/102456.jpg)');
                 msgDiv.innerText ="player 2 Your turn choose ";
                 return player = "O";
             }
             if(player1 =="O")
             {   
-                $(event.target).text(player1);
+                $(event.target).text(player1).animate({fontSize: "6em"});
                 msgDiv.innerText ="player 1 Your turn choose ";
-                //msgDiv.innerText ="player 2 Your Turn choose "+player1;
                 return player = "X";
             }
            
@@ -82,7 +86,7 @@ function fisrt_role(player1)
                 if(notempty($(boxs[i]).text() , $(boxs[i+3]).text() , $(boxs[i+6]).text()))
                 {
                     if($(boxs[i]).text() == $(boxs[i+3]).text() && $(boxs[i+3]).text() == $(boxs[i+6]).text()){
-                        $(boxs).off('click')
+                        $(boxs).off('click');
                         //return msg1Div.innerText="You Win player "+user;
                         return $(boxs[i]).text();
                     }
@@ -107,57 +111,50 @@ function fisrt_role(player1)
                     return $(boxs[2]).text();
                 }
             }
-            // let row = [0,1,2,3,4,5,6,7,8];
-            // for(let i =0;i<=row.length;i = i+3)
-            // {
-            //     if(notempty($(boxs[row[i]]).text(), $(boxs[row[i+1]]).text() , $(boxs[row[i+2]]).text()))
-            //     {    
-            //         if($(boxs[row[i]]).text()==$(boxs[row[i+1]]).text()&& $(boxs[row[i+1]]).text() == $(boxs[row[i+2]]).text())
-            //         {
-            //             $(boxs).off('click');
-            //              return $(boxs[row[i]]).text();
-            //         }
-            //     }
-            // }
+            let row = [0,1,2,3,4,5,6,7,8];
+            for(let i =0;i<=row.length;i = i+3)
+            {
+                if(notempty($(boxs[row[i]]).text(), $(boxs[row[i+1]]).text() , $(boxs[row[i+2]]).text()))
+                {    
+                    if($(boxs[row[i]]).text()==$(boxs[row[i+1]]).text()&& $(boxs[row[i+1]]).text() == $(boxs[row[i+2]]).text())
+                    {
+                        $(boxs).off('click');
+                         return $(boxs[row[i]]).text();
+                    }
+                }
+            }
+            
             
             // check first row if match
-            if(notempty($(boxs[0]).text(), $(boxs[1]).text() , $(boxs[2]).text()))
-            {    
-                if($(boxs[0]).text()==$(boxs[1]).text()&& $(boxs[1]).text() == $(boxs[2]).text())
-                {
-                    $(boxs).off('click');
-                     return $(boxs[0]).text();
-                }
-            }
-            //check second row if match
-            if(notempty($(boxs[3]).text(), $(boxs[4]).text() , $(boxs[5]).text()))
-            {    
-                if($(boxs[3]).text()==$(boxs[4]).text()&& $(boxs[4]).text() == $(boxs[5]).text())
-                {
-                    $(boxs).off('click');
-                     return $(boxs[3]).text();
-                }
-            }
+            // if(notempty($(boxs[0]).text(), $(boxs[1]).text() , $(boxs[2]).text()))
+            // {    
+            //     if($(boxs[0]).text()==$(boxs[1]).text()&& $(boxs[1]).text() == $(boxs[2]).text())
+            //     {
+            //         $(boxs).off('click');
+            //          return $(boxs[0]).text();
+            //     }
+            // }
+            // //check second row if match
+            // if(notempty($(boxs[3]).text(), $(boxs[4]).text() , $(boxs[5]).text()))
+            // {    
+            //     if($(boxs[3]).text()==$(boxs[4]).text()&& $(boxs[4]).text() == $(boxs[5]).text())
+            //     {
+            //         $(boxs).off('click');
+            //          return $(boxs[3]).text();
+            //     }
+            // }
       
-            // //check third row if match
-            if(notempty($(boxs[6]).text(), $(boxs[7]).text() , $(boxs[8]).text()))
-            {    
-                if($(boxs[6]).text()==$(boxs[7]).text()&& $(boxs[7]).text() == $(boxs[8]).text())
-                {
-                    $(boxs).off('click');
-                     return $(boxs[6]).text();
-                }
-            }
+            // // //check third row if match
+            // if(notempty($(boxs[6]).text(), $(boxs[7]).text() , $(boxs[8]).text()))
+            // {    
+            //     if($(boxs[6]).text()==$(boxs[7]).text()&& $(boxs[7]).text() == $(boxs[8]).text())
+            //     {
+            //         $(boxs).off('click');
+            //          return $(boxs[6]).text();
+            //     }
+            // }
       
-      
-      
-       
-
    }
-//check row by row if X or O in 3 row
-
-
-
 
 // function to check if all 3 varblie not equal to empty string
 function notempty(box1,box2,box3)
@@ -172,8 +169,8 @@ function resetBoxs(){
     
     for(let i =0;i<boxs.length;i++){
         
-        boxs[i].innerText = "";
-        
+        $(boxs[i]).text("");
+        $(boxs[i]).removeAttr("style");
 
      }
      player = "X";
@@ -181,7 +178,9 @@ function resetBoxs(){
      boxs.one("click",start);
 
 }
-
-boxs.one("click",start);
+//add listener to the game bord with Jquery
+$(boxs).one("click",start);
+//add listener for restart button
 resetBtn.addEventListener("click",resetBoxs);
+
 
